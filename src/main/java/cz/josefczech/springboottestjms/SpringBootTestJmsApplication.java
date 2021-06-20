8b751dc247275726071c4fee96183d5e0da62efd
Josef Cech <czech.josef@gmail.com>
2021-06-20 11:43:06 +0200

    Init commit

---
+++ src/main/java/cz/josefczech/springboottestjms/SpringBootTestJmsApplication.java
@@ -0,0 +1,13 @@
+package cz.josefczech.springboottestjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/josefczech/springboottestjms/SpringBootTestJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.josefczech.springboottestjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
