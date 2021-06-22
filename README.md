adc8d87c2d8563dba63c11fda521b3e66614e0a5
Josef Cech <czech.josef@gmail.com>
2021-06-22 18:19:41 +0200

    JmsCaptorTestExecutionListener: Stop/start listeners so they run only in tests attached to them

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 JmsCaptor is a small utility to capture published JMS messages in Spring Boot integration tests.
 
 Please note: This is my weekend project for learning more about Spring Boot internals.
-It's functional but I do not use it at any commercial project.
+It's functional, but I do not use it at any commercial project.
 
 ## Usage
 
--- src/main/java/cz/josefczech/springboot/test/jmscaptor/config/JmsCaptorTestExecutionListener.java
+++ src/main/java/cz/josefczech/springboot/test/jmscaptor/config/JmsCaptorTestExecutionListener.java
@@ -1,7 +1,9 @@
 package cz.josefczech.springboot.test.jmscaptor.config;
 
 import cz.josefczech.springboot.test.jmscaptor.JmsCaptor;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.jms.config.JmsListenerEndpointRegistry;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
 import org.springframework.util.ReflectionUtils;
@@ -14,6 +16,36 @@ import java.util.Map;
  * and cleaning up the captor before every run.
  */
 class JmsCaptorTestExecutionListener extends AbstractTestExecutionListener {
+
+    /**
+     * Start previously stopped listeners if context is cached and reused.
+     * @param testContext Spring test context
+     */
+    @Override
+    public void beforeTestClass(TestContext testContext) {
+        ApplicationContext applicationContext = testContext.getApplicationContext();
+        try {
+            JmsListenerEndpointRegistry listenerRegistry = applicationContext.getBean(JmsListenerEndpointRegistry.class);
+            listenerRegistry.start();
+        } catch (NoSuchBeanDefinitionException e) {
+            // ignore (this test does not need JMS)
+        }
+    }
+
+    /**
+     * Stop previously started listeners (so they do not affect other tests).
+     * @param testContext Spring test context
+     */
+    @Override
+    public void afterTestClass(TestContext testContext) {
+        ApplicationContext applicationContext = testContext.getApplicationContext();
+        try {
+            JmsListenerEndpointRegistry listenerRegistry = applicationContext.getBean(JmsListenerEndpointRegistry.class);
+            listenerRegistry.stop();
+        } catch (NoSuchBeanDefinitionException e) {
+            // ignore (this test does not need JMS)
+        }
+    }
 
     /**
      * The method prepares test instance by setting JmsCaptor to all fields.
